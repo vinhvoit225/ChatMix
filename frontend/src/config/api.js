3712730b4772a7588824a4424010590951c530e2
Vo The Vinh <vinhvoit225@gmail.com>
2025-08-14 17:38:28 +0700

    first commit

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-  // API_BASE_URL: 'http://chatmix.apex-intel.cloud/api',
-  // WS_BASE_URL: 'ws://chatmix.apex-intel.cloud',
+  API_BASE_URL: 'https://chatmix.apex-intel.cloud/api',
+  WS_BASE_URL: 'wss://chatmix.apex-intel.cloud',
 
-  API_BASE_URL: 'http://localhost:8082/api',
-  WS_BASE_URL: 'ws://localhost:8082',
+  // API_BASE_URL: 'http://localhost:8082/api',
+  // WS_BASE_URL: 'ws://localhost:8082',
 };
 
 export default CONFIG;
